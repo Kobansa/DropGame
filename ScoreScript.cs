fb7bd121cd44472e99294b266aac20dc435454ae
rinakoisaka <33512159+rinakoisaka@users.noreply.github.com>
2021-11-26 13:53:50 +0900

    Add files via upload

---
+++ ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int Score; //変数
+    Text ScoreText; //特典の文字の変数
+    //public GameObject Basket;
+    void Start()
+    {
+        ScoreText = GameObject.Find("Text").GetComponent<Text>(); //ScoreTextをTextにする
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "Score" + Score.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Apple")
+        {
+            Score += 100;
+        }
+    }
+}
---
+++ TimerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float Timer;
+    public Text TimerText;
+
+    private float num1;
+    void Quit()
+    {
+        UnityEngine.Application.Quit();
+    }
+        void Start()
+    {
+        Timer = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        num1 = Mathf.Floor(Timer); //小数点以下は切り捨て
+        TimerText.text = "Time:"+ num1.ToString();
+
+        if(Timer >= 0)
+        {
+            Timer -= Time.deltaTime * 1; //Timerの時間を減らす
+        }
+        else if(Timer <= 0)
+        {
+            Timer = 0;
+            print("Time Up!!");
+            //Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;   // UnityEditorの実行を停止する処理
+
+            Application.Quit();                                // ゲームを終了する処理
+        }
+    }
+}
